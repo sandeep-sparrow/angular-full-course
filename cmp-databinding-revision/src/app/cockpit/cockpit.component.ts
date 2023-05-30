6e7e9f972b918c651561d44a0107f37fd6f5394a
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-05-30 13:05:06 +0530

    added custom event alias bpCreated

--- cmp-databinding-revision/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-revision/src/app/cockpit/cockpit.component.ts
@@ -8,7 +8,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class CockpitComponent {
 
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   newServerName = '';
   newServerContent = '';
