86b46084fdcce7909512d4ab99cc27bfc7eeb18f
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-28 01:08:10 +0530

    added canActivate & canActivateChild for route guarding

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -7,15 +7,21 @@ import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuardService } from "./auth-guard.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
+  { path: 'users',
+    component: UsersComponent,
+    children: [
       { path: ':id/:name', component: UserComponent }
     ]
   },
 
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers',
+    canActivateChild: [AuthGuardService],
+    component: ServersComponent,
+    children: [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ]
