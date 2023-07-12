33853cea85e71326703164d84c598a640ab6517c
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-12 10:12:09 +0530

    added Routing-start-revision

---
+++ routing-start-revision/src/app/users/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private activeRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.user = {
+      id: this.activeRoute.snapshot.params['id'],
+      name: this.activeRoute.snapshot.params['name']
+    };
+    this.activeRoute.params.subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+    )
+  }
+
+}
